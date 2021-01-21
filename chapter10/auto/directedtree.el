e1fac3d9187faed41cc8478379660a42e9a752e0
jianwen <chenjianwen@hit.edu.cn>
2021-01-22 00:36:20 +1000

    chapter10 modified

---
+++ chapter10/auto/directedtree.el
@@ -0,0 +1,15 @@
+(TeX-add-style-hook
+ "directedtree"
+ (lambda ()
+   (add-to-list 'LaTeX-verbatim-environments-local "semiverbatim")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-add-symbols
+    "raggedright"))
+ :latex)
+
